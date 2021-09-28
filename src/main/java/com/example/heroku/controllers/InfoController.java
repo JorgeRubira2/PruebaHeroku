c696082457792b4535a5783ed5f78b212e3249c8
JorgeRubira <jorgesub@gmail.com>
2021-09-28 06:25:20 +0200

    Merge origin/master into HEAD
    Conflicts:
    	.gitignore
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java

--- src/main/java/com/example/heroku/controllers/InfoController.java
+++ src/main/java/com/example/heroku/controllers/InfoController.java
@@ -4,6 +4,7 @@ package com.example.heroku.controllers;
 import com.example.heroku.entities.Persona;
 import com.example.heroku.repositories.PersonaRepository;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,9 @@ public class InfoController {
 
     @GetMapping("/info")
     public List<Persona> obtenerPersonas(){
+        Persona p=new Persona();
+        p.setNombre(UUID.randomUUID().toString());
+        repo.save(p);
         return repo.findAll();
     }
 }
